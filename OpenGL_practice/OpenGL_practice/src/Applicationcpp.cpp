4049b5860bb811f31b6fbc1b0db5946fc3b48805
Uraharadono <35668552+Uraharadono@users.noreply.github.com>
2018-03-25 13:24:03 +0200

    Video 7, writing a shader in OpenGl

--- OpenGL_practice/OpenGL_practice/src/Applicationcpp.cpp
+++ OpenGL_practice/OpenGL_practice/src/Applicationcpp.cpp
@@ -1,6 +1,60 @@
+/*
+ * OPEN GL DOCUMENTATION SITE: http://docs.gl/
+ */
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	// Error handling
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+		// On govori ovdje da hoce na stacku da napravi char array, nece pokazivac koji bi bio na heap-u jer ga mora kasnije pocistiti.
+		// Ja se ne slazem sa njim, ali da bi mi bilo lakse pratiti video-e i ja cu napraviti kao i on.
+		// char* message = new char[length];
+
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " <<
+			(type == GL_VERTEX_SHADER ? "vertext" : "fragment")
+			<< " shader." << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+	
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	// delete intermediates
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 
 int main(void)
@@ -29,6 +83,7 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	// Vertex buffers and layouts in open GL
 	float positions[6] = {
 		-0.5f, -0.5f,
 		0.0f, 0.5f,
@@ -38,9 +93,32 @@ int main(void)
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-
+	// Vertex attributes and layouts in open GL
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 	glEnableVertexAttribArray(0);
+
+	// Creating shaders
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -64,6 +142,9 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	// Delete shader in the end
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
